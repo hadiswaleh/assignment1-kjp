dd8c3593d6095da78358e43137434171df1edc4d
HADI <hadiswaleh887@gmail.com>
2022-03-29 12:19:50 +0500

    Signed-off-by: HADI Signed-off-by: HADI

--- assignment.jsx
+++ assignment.jsx
@@ -1,37 +1,59 @@
-    const Emp = [{
-        id : 1,
-        name : 'Hadi',
-        designation : 'Account Executive'
-    },{
-        id : 2,
-        name : 'Hanzala',
-        designation : 'CFO'
-    },{
-        id : 3,
-        name : 'Aamir',
-        designation : 'CEO'
-    },{
-        id : 4,
-        name : 'Akhai',
-        designation : 'IT'
-    },{
-        id : 5,
-        name : 'Asim',
-        designation : 'Director IT'
-    },]
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
     
-    
-// with map function
-    let result = Emp.map(function(x){
-        return x.name
-    })    
-// with forEach function
-     let xa = [];
-     let result12 = Emp.forEach(function(key , Index){
-        xa.push(key.id)
-     })
-    
-    console.log(xa);
-    console.log(result)
+   
+let Emp = [{
+    id : 1,
+    name : 'Hadi',
+    designation : 'Account Executive'
+},{
+    id : 2,
+    name : 'Haris',
+    designation : 'Team Lead'
+},{
+    id : 3,
+    name : 'Aamir',
+    designation : 'CEO'
+},{
+    id : 4,
+    name : 'Hunaina',
+    designation : 'Devops'
+},{
+    id : 5,
+    name : 'Asim',
+    designation : 'Director IT'
+},]
 
-    
+ 
+// with map function
+let printKey = Emp.map(function(x , Index){
+    return Index
+})
+let printValue = Emp.map(function(x){
+    return x.name
+})
+
+//map
+console.log("Code using Map Function")
+console.log(printKey)
+console.log(printValue)
+
+// with forEach function
+ let printKey1 = [];
+ let result12 = Emp.forEach(function(key , Index){
+    printKey1.push(Index)
+ })
+ 
+  let printValue1 = [];
+ let result13 = Emp.forEach(function(key , Index){
+    printValue1.push(key.name)
+ })   
+
+
+// foreach
+console.log("Code using forEach Function")
+console.log(printKey1);
+console.log(printValue1);
+
+
+
